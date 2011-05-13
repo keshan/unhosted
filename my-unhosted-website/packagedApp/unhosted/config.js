b6c223241d33abfa50aef305d449e0784819c5b0
Michiel de Jong <michiel@unhosted.org>
2011-05-13 19:12:33 +0200

    correct clientId and appUrl in hardcoded config

--- my-unhosted-website/packagedApp/unhosted/config.js
+++ my-unhosted-website/packagedApp/unhosted/config.js
@@ -2,9 +2,9 @@ var appBaseUrl = 'chrome-extension://gdkemcflfdajofefmfdbglmkkdjdbnbn/';
 var installationType = 'app';
 
 var config = {
-	appUrl: '',
+	appUrl: appBaseUrl,
 	loginUrl: 'unhosted/login.html',
-	callbackUrl: appBaseUrl+ 'unhosted/callback.html',
-	appName: 'My Favourite Sandwich',
+	callbackUrl: appBaseUrl + 'unhosted/callback.html',
+	clientId: appBaseUrl,
 	dataScope: 'myfavouritesandwich.org',
 };
