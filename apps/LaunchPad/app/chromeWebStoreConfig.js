23c166582438810410d3ce5fcb7d7160fe5cf190
Michiel de Jong <michiel@unhosted.org>
2011-05-22 23:57:45 +0200

    adding LaunchPad app

---
+++ apps/LaunchPad/app/chromeWebStoreConfig.js
@@ -0,0 +1,10 @@
+var appBaseUrl = 'chrome-extension://icnlmeafeacofpkfbaaphcknoclngkdh';
+var installationType = 'app';
+
+var config = {
+	appUrl: appBaseUrl + '/index.html',
+	loginUrl: '/unhosted/login.html',
+	callbackUrl: appBaseUrl + '/unhosted/callback.html',
+	clientId: appBaseUrl,
+	dataScope: 'myfavouritesandwich.org',
+};
---
+++ apps/LaunchPad/unhosted/config.js
@@ -0,0 +1,11 @@
+var config = {
+	appUrl: appBaseUrl + '/',
+	doUrl: appBaseUrl + '/unhosted/do.php',
+	loginUrl: appBaseUrl + '/unhosted/login.html',
+	registerUrl: appBaseUrl + '/unhosted/register.html',
+	callbackUrl: appBaseUrl + '/unhosted/callback.html',
+	clientId: document.location,
+	dataScope: 'sandwiches',
+	homeDomain: null
+}
+
