f747952bf2a4b529c6215894d4ffe5e37614e1a7
Michiel de Jong <michiel@unhosted.org>
2011-05-22 23:34:31 +0200

    fix wallet install

--- admin/install_wallet.php
+++ admin/install_wallet.php
@@ -1,11 +1,11 @@
 <?php
 if(
-	mkdir('../wallet') &&
-	copy('wallet.php', '../wallet/index.php') &&
-	mkdir('../wallet/wallets') &&
-	copy('wallets.htaccess', '../wallet/wallets/.htaccess')
+	(file_exists('../wallet') || mkdir('../wallet')) &&
+	(file_exists('../wallet/index.php') || copy('wallet.php', '../wallet/index.php')) &&
+	(file_exists('../wallet/wallets') || mkdir('../wallet/wallets')) &&
+	(file_exists('../wallet/wallets/.htaccess') || copy('wallets.htaccess', '../wallet/wallets/.htaccess'))
 	) {
-	echo 'OK';
+	echo 'ok';
 } else {
 	echo 'could not create wallet dir';
 }
